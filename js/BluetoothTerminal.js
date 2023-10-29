63fd010a3dfd5bdd99bb662b4f261b8f0f143cf9
Borgling <111878706+Borgling@users.noreply.github.com>
2023-10-29 17:22:40 +0000

    Update BluetoothTerminal.js

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -316,7 +316,7 @@ class BluetoothTerminal {
           this._log('Characteristic found');
 
           this._characteristic = characteristic; // Remember characteristic.
-	 characteristic.writeValueWithoutResponse("TEST");	
+	 characteristic.writeValueWithoutResponse(new TextEncoder().encode("TEST"));	
           return this._characteristic;
         });
   }
